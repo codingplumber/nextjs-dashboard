047daa21d163a269cf32f53245ffa2bf123935b9
Stickel <E40019908@adxuser.com>
2024-02-01 15:11:13 -0500

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
+
+// This is called a root layout and is required. Any UI you add to the root layout will be shared across all pages in your 
+// application. You can use the root layout to modify your <html> and <body> tags, and add metadata (you'll learn more about 
+// metadata in a later chapter).
